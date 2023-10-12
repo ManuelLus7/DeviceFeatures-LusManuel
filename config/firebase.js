2b3520f16967d9fe5a30901142ac9e99223bd963
ManuelLus7 <ManuelLus7@users.noreply.github.com>
2023-10-11 22:37:54 -0300

    Corrigiendo Commint

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import {getAuth} from 'firebase/auth'
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyCKSbFT__lnAenwiNIw9EZW9aIdlT-KB_s",
   authDomain: "wotton-69aa3.firebaseapp.com",
@@ -14,11 +11,7 @@ const firebaseConfig = {
   messagingSenderId: "993698469211",
   appId: "1:993698469211:web:f53dc544be8ceebc37b67e"
 };
-// these keys won't work because i've removed the app from firebase
-// add your app on firebase, copy firebaseConfig here, enable email/password auth
-// and test the app ;)
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
